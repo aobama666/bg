703f5449c896ac9e71d834ca227c79d95ab2de6b
wubin <wubin406@epri.sgcc.com.cn>
2018-10-09 16:25:56 +0800

    工时推送接口调整：期间内有效项目，若没有项目负责人工时，生成一条为0的记录

--- src/main/java/com/sgcc/bg/mapper/BgInterFaceMapper.java
+++ src/main/java/com/sgcc/bg/mapper/BgInterFaceMapper.java
@@ -71,5 +71,19 @@ public interface BgInterFaceMapper {
 			@Param("endDate")String endDate,
 			@Param("monthName")String monthName,
 			@Param("update_time")String update_time);
-
+	
+	/**
+	 * 验证基本信息是否存在
+	 * @param WT_SEASON 期间    Y,S1,S2,S3,S4,M1,M2,M3,M4,M5,M6,M7,M8,M9,M10,M11,M12
+	 * @param yearName  期间为Y使用：年份  yyyy
+ 	 * @param startDate 期间为季度使用：S1,S2,S3,S4  开始时间  yyyy-MM-dd
+	 * @param endDate   期间为季度使用：S1,S2,S3,S4   结束时间 yyyy-MM-dd
+	 * @param monthName 期间为月份使用：M1,M2,M3,M4,M5,M6,M7,M8,M9,M10,M11,M12  yyyy-MM
+	 * @return
+	 */
+	List<Map<String, Object>> getInterfaceBaseData(@Param("WT_SEASON")String WT_SEASON,
+			@Param("yearName")String yearName,
+			@Param("startDate")String startDate,
+			@Param("endDate")String endDate,
+			@Param("monthName")String monthName);
 }
