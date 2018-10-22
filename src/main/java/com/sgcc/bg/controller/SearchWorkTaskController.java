3ed1a8a46a26a805b28be40eaee1abb32b442f7e
lilinlin <epri-dalin@admin>
2018-10-22 14:54:08 +0800

    小改动

--- src/main/java/com/sgcc/bg/controller/SearchWorkTaskController.java
+++ src/main/java/com/sgcc/bg/controller/SearchWorkTaskController.java
@@ -1,26 +1,26 @@
 package com.sgcc.bg.controller;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
 import com.alibaba.fastjson.JSON;
 import com.sgcc.bg.common.CommonUser;
 import com.sgcc.bg.common.DateUtil;
 import com.sgcc.bg.common.ExportExcelHelper;
-import com.sgcc.bg.common.Rtext;
 import com.sgcc.bg.common.UserUtils;
 import com.sgcc.bg.common.WebUtils;
-import com.sgcc.bg.model.Recode;
 import com.sgcc.bg.service.DataDictionaryService;
 import com.sgcc.bg.service.IStaffWorkbenchService;
 import com.sgcc.bg.service.SearchWorkTaskService;
