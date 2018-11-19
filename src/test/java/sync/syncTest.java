12456b93b18c86368696985d73be91fd105382c7
tonny <tonny@tonny-PC>
2018-11-19 10:20:10 +0800

    1：修改获取专责权限部门的方法为调用接口 2：修改bug，根据部门获取人员应该是根据报工表获取，而不是根据当前部门下人员信息

--- src/test/java/sync/syncTest.java
+++ src/test/java/sync/syncTest.java
@@ -4,30 +4,29 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 
-import com.sgcc.bg.service.IBGService;
 import com.sgcc.bg.service.SyncService;
 
 import config.TestConfig;
 
 
 public class syncTest {
-	/*public static ApplicationContext ctx;
-	public SyncService sync = ctx.getBean(SyncService.class);*/
+	public static ApplicationContext ctx;
+	public SyncService sync = ctx.getBean(SyncService.class);
 
-	/*@BeforeClass
+	@BeforeClass
 	public static void initConfig(){
 		ctx=TestConfig.getApplicationContext("src/main/resources/applicationContext.xml");
-	}*/
+	}
 	@Test
 	public void sync() {
-		/*System.out.println("------start-----");
+		System.out.println("------start-----");
 		sync.syncErpSyncData();
 		sync.syncUserOrganRelationData();
-		System.out.println("------end-----");*/
-		System.out.println("------start-----");
+		System.out.println("------end-----");
+		/*System.out.println("------start-----");
 		StringBuilder sb=new StringBuilder();
 		sb.append("1").append("2");
 		System.out.println(sb.length());
-		System.out.println(sb.deleteCharAt(1).toString());
+		System.out.println(sb.deleteCharAt(1).toString());*/
 	}
 }
