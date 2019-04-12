9c91b65e0b533dbd546b1c33371f5af7a58d903b
shishanshan <shishanshan@qq.com>
2019-04-12 10:45:30 +0800

    前端提交

---
+++ src/main/webapp/yszx/js/laboratory/roomDetailInfo.js
@@ -0,0 +1,107 @@
+var roomDetailInfo = {};
+roomDetailInfo.saveBtnClickFlag = 0;//保存按钮点击事件
+roomDetailInfo.saveInfoFlag = true;//页面数据保存事件
+ 
+/* 保存信息库信息 */
+roomDetailInfo.forSave_pro= function(){
+	/* 验证必填项     */
+	var validNull = dataForm.validNullable();
+	if(!validNull){
+		roomDetailInfo.saveBtnClickFlag = 0;
+		return;
+	}
+	var checkLength = dataForm.checkLength();
+	if(!checkLength){
+		roomDetailInfo.saveBtnClickFlag = 0;
+		return;
+	}
+	 
+}
+function checkStartDate(startDate){
+	var result = {};
+	var currentYear=new Date().getFullYear();
+	if($("select[name=category]").val()=="JS" && startDate.substr(0,4)!=currentYear){
+		result.result = false;
+		result.info = "技术服务非项目不能跨年；";
+	}else{
+		result.result = true;
+		result.info = "";
+	}
+	return result;
+}
+
+function checkEndDate(endDate){
+	var result = {};
+	var currentYear=new Date().getFullYear();
+	var startDate = $("input[name=startDate]").val();
+	if($("select[name=category]").val()=="JS" && endDate.substr(0,4)!=currentYear){
+		result.result = false;
+		result.info = "技术服务非项目不能跨年；";
+		return result;
+	}
+	if((new Date(endDate.replace(/-/g,"\/")))>(new Date(startDate.replace(/-/g,"\/")))){
+		result.result = true;
+		result.info = "";
+	}else{
+		result.result = false;
+		result.info = "非项目结束时间必须大于非项目开始时间；";
+	}
+	return result;
+}
+
+function checkNumberFormat(planHours){
+	var result = {};
+	var reg=/^([0-9]|[1-9][0-9]{0,7})$/;
+	if($.trim(planHours)!="" && !reg.test(planHours)){
+		result.result = false;
+		result.info = "必须为8位以内整数；";
+	}else{
+		result.result = true;
+		result.info = "";
+	}
+	return result;
+}
+
+function checkDateRange(date){
+	var result = {};
+	if(getDate($("input[name='startDate']").val())<=getDate(date) && getDate($("input[name='endDate']").val())>=getDate(date)){
+		result.result = true;
+		result.info = "";
+	}else{
+		result.result = false;
+		result.info = "日期超范围；";
+	}
+	return result;
+}
+function checkOrganFormat(deptName){
+	var result = {};
+	if($.trim(deptName) =="中国电力科学研究院有限公司"  ){
+		result.result = false;
+		result.info = "组织信息不能为'中国电力科学研究院有限公司'";
+	}else{
+		result.result = true;
+		result.info = "";
+	}
+	return result;
+}
+
+
+/*时间控件change事件*/
+function datePackerChange(dp, el) {
+    var oldDate = dp.cal.getDateStr();
+    var newDate = dp.cal.getNewDateStr();
+    if (oldDate != newDate) {
+        $(el).removeClass("validRefuse").addClass("validPass");
+    }
+}
+/*时间控件change事件*/
+roomDetailInfo.datePackerChange = function(dp, el) {
+    var oldDate = dp.cal.getDateStr();
+    var newDate = dp.cal.getNewDateStr();
+    if (oldDate != newDate) {
+        $(el).removeClass("validRefuse").addClass("validPass");
+    }
+}
+
+
+
---
+++ src/main/webapp/yszx/js/laboratory/roomList.js
@@ -0,0 +1,368 @@
+
+//定义一个
+var roomList = {};
+var dicts = {};
+var dataItems = new Array();
+var index = 0;
+roomList.btn_type_flag = 0;
+$(function(){
+	//‘新增’页面，院领导姓名多选下拉框
+	 $(".tree-data").combotree('loadData',[{
+		 id:1,
+		 text:'姓名',
+		 children:[{
+			 id:11,
+			 text:'王晓红',
+		 }]
+		 }]);
+	roomList.initDataGrid();
+	var classQuery = $(".changeQuery");
+	/* 输入框的change事件，在输入过程中自动查询  */
+	$(".changeQuery").change(function(e){
+		roomList.query();
+	});
+	$(".inputQuery").on("input",function(e){
+		var valLength = e.target.value.length;
+		if(valLength>3){
+			roomList.query();
+		}
+	});
+	//回车键出发搜索按钮
+	$("body").keydown(function () {
+	    if (event.keyCode == "13") {
+	    	dataItems = new Array();
+			index = 0;
+	        $("#datagrid").datagrid("seach");
+	        return false;
+	    }
+	});
+	roomList.btn_type_flag = 0;
+});
+
+/*  start  列表查询  */
+roomList.query = function(){
+	dataItems = new Array();
+	index = 0;
+	$("#datagrid").datagrid("seach");
+}
+/*  end  列表查询  */
+
+/* 演示中心管理-初始化列表界面  */
+roomList.initDataGrid = function(){
+	    $("#datagrid").datagrid({
+		url: "/bg/nonProject2/initPage",
+		type: 'POST',
+		form:'#queryForm',
+		 
+		pageSize:50,
+		tablepage:$(".tablepage"),//分页组件
+		successFinal:function(){
+			roomList.resize();
+		},
+		callBackFunc:function(){
+			roomList.initItems();
+		},
+		columns: [
+		/*  {name: '序号',style:{width:"2%"}, data: 'RN'},    */      
+		  {name: '',style:{width:"2%"}, data: 'id',checkbox:true, forMat:function(row){
+			  dataItems[index] = row;//将一行数据放在一个list中
+			  return '<input type="checkbox" name="oneCheck"  index = "'+(index++)+'"  value="'+(row.id)+'"/>';
+		 	}
+		  },
+		  {name: '申请单号',style:{width:"5%"}, data: 'projectNumber',forMat:function(row){
+			  return "<a title = '"+row.projectNumber+"' style='width:150px;" + 
+				  		"text-align:left;display:block;" +
+				  		"white-space: nowrap;" +
+				  		"text-overflow: ellipsis;" +
+				  		"overflow: hidden;' id = '"+row.id+"'" +
+				  		"href = 'javascript:void(0)' onclick = roomList.forDetails('"+row.id+"')>"+row.projectNumber+"</a>";
+				  		 
+		  }},
+		  {name: '申请时间', style:{width:"8%"},data: 'startDate'},
+		  {name: '申请部门（单位）',style:{width:"10%"}, data: 'projectName',forMat:function(row){
+			  if(row.projectName){
+				  	return "<span title='"+row.projectName+"' style='width:150px;text-align:left;display:block;overflow:hidden;white-space: nowrap;text-overflow: ellipsis;'>"+row.projectName+"</span>"
+				  }else{
+					  return "";
+			 }
+		  }},
+		 
+		  {name: '主要参观领导', style:{width:"10%"},data: 'projectIntroduce',forMat:function(row){
+			  if(row.projectIntroduce){
+				  	return "<span title='"+row.projectIntroduce+"' style='width:300px;text-align:left;display:block;overflow:hidden;white-space: nowrap;text-overflow: ellipsis;'>"+row.projectIntroduce+"</span>"
+				  }else{
+					  return "";
+			     }
+		  }},
+		  {name: '院内陪同人员', style:{width:"10%"},data: 'projectIntroduce',forMat:function(row){
+			  if(row.projectIntroduce){
+				  	return "<span title='"+row.projectIntroduce+"' style='width:300px;text-align:left;display:block;overflow:hidden;white-space: nowrap;text-overflow: ellipsis;'>"+row.projectIntroduce+"</span>"
+				  }else{
+					  return "";
+			     }
+		  }},
+		  {name: '参观开始时间', style:{width:"8%"},data: 'startDate'},
+		  {name: '参观结束时间', style:{width:"8%"},data: 'endDate'},
+		  {name: '审批状态',style:{width:"7%"},data: 'projectStatus'  ,forMat:function(row){
+			   
+			      if(row.projectStatus=="0"){
+				  	return "未启动"
+				  }else if(row.projectStatus=="1"){
+					return "进行中";
+			      }else if(row.projectStatus=="2"){
+					return "暂停";
+			      }else if(row.projectStatus=="3"){
+					return "已结束";
+			      }else if(row.projectStatus=="4"){
+					return "废止";
+			      }
+		      }
+		  },
+		  {name: '联系人', style:{width:"6%"},data: 'planHours'},
+		  {name: '联系方式', style:{width:"8%"},data: 'planHours'}
+		  
+		]
+	});
+
+}	
+	
+	/*演示中心管理-查看 */	
+	roomList.forDetails = function (id){
+		var url = "/bg/nonProject2/pro_details?proId="+id;
+			parent.layer.open({
+				type:2,
+				title:'<h4 style="height:42px;line-height:25px;">项目前期工作维护-查看</h4>',
+				area:['40%','60%'],
+				fixed:false,//不固定
+				maxmin:true,
+				content:url, 
+			});
+	}
+	/*演示中心管理-新增 */
+	roomList.addEvent = function (){
+		var url = "/bg/nonProject2/pro_add"
+			parent.layer.open({
+				type:2,
+				title:'<h4 style="height:42px;line-height:25px;">项目前期工作维护-新增</h4>',
+				area:['40%','60%'],
+				fixed:false,//不固定
+				maxmin:true,
+				content:url, 
+			});
+	}
+		
+	/* 演示中心管理-修改*/
+	roomList.updateEvent = function(){
+		var checkedItems = dataGrid.getCheckedItems(dataItems);
+		if(checkedItems.length==0){
+			messager.tip("请选择要操作的数据",1000);
+			return;
+		}else if(checkedItems.length>1){
+			messager.tip("每次只能修改一条数据",2000);
+			return;
+		}
+		if(checkedItems[0].projectStatus=="3" || checkedItems[0].projectStatus=="4"){
+			messager.tip("该无法修改",2000);
+			return;
+		}
+		var id = dataGrid.getCheckedIds();
+		var url = "/bg/nonProject2/pro_update?proId="+id;
+		parent.layer.open({
+			type:2,
+			title:'<h4 style="height:42px;line-height:42px;">项目前期工作维护-修改 </h4>',
+			area:['40%','60%'],
+			fixed:false,//不固定
+			maxmin:true,
+			content:url, 
+		});
+	}
+		
+/*  start 全选、取消全选 */
+$(".check_").change(function(){
+	if(this.checked==true){
+		var checkBoxs=$("input:checkbox[name=oneCheck]");
+		checkBoxs.each(function(i){
+			checkBoxs[i].checked=true;
+		});
+	}else{
+		var checkBoxs=$("input:checkbox[name=oneCheck]");
+			checkBoxs.each(function(i){
+				checkBoxs[i].checked=false;
+		});
+	}
+});
+/*  end 全选、取消全选 */
+
+/* 初始化dataItems */
+roomList.initItems = function(){
+	dataItems = new Array();
+	index = 0;
+}
+
+
+
+/* start 删除方法*/
+roomList.delEvent = function(){
+	var delDataInfo = {};
+	var checkedIds = dataGrid.getCheckedIds();
+	delDataInfo.LBIDS = checkedIds;
+	if(checkedIds.length==0){
+		messager.tip("请选择要操作的数据",1000);
+		return;
+	}
+	$.messager.confirm( "删除提示", "确认删除选中数据吗",
+		function(r){
+			if(r){
+				$.ajax({
+				    url: "/newtygl/laboratory/roomDel",//删除
+					type: "post",
+					dataType:"json",
+					contentType: 'application/json',
+					data:JSON.stringify(delDataInfo),//传递的需要删除的数据
+					success: function (p_content) {
+						if( p_content.P_DATA == "SUCCESS"){
+							messager.tip("删除成功",1000);
+							roomList.query();
+						}else{
+							messager.tip("删除失败",1000);
+							roomList.query();
+						}
+					}
+				});
+			}
+		}
+	);
+}
+/* end 删除方法*/
+
+
+roomList.resize=function(){
+	var height=$("body").height()-$(".sheach").height()-$("#funcBtn").height()-65;
+	$("#datagrid>div").css({"height":height});
+}
+$(window).resize(function(){
+	roomList.resize();
+})
+
+
+roomList.openRoomDetail = function (id,type){
+	var url = "/bg/nonProject2/pro_details?proId="+id;
+	if(parent.layer.open){
+		parent.layer.open({
+			type:2,
+			title:'<h4 style="height:42px;line-height:42px;">实验室信息</h4>',
+			area:['90%','90%'],
+			fixed:false,//不固定
+			maxmin:true,
+			content:url,
+			cancel:function(index,layero){
+				if(type=="edit"){
+					var iframeWin = window[layero.find('iframe')[0]['name']];
+					var layerViewSaveFlag = iframeWin.roomDetailInfo.saveInfoFlag;
+					if(!layerViewSaveFlag){
+						if(confirm("数据改变还未保存，是否关闭弹框？")){
+							return true;
+						}else{
+							return false;
+						}
+					}
+				}
+			},
+			end:function(){
+				dataItems = new Array();
+				index = 0;
+				$("#datagrid").datagrid("refresh");
+			}
+		});
+	}else{
+		layer.open({
+			type:2,
+			title:'<h4 style="height:42px;line-height:42px;">实验室信息</h4>',
+			area:['90%','90%'],
+			fixed:false,//不固定
+			maxmin:true,
+			content:url,
+			cancel:function(index,layero){
+				if(type=="edit"){
+					var iframeWin = window[layero.find('iframe')[0]['name']];
+					var layerViewSaveFlag = iframeWin.roomDetailInfo.saveInfoFlag;
+					if(!layerViewSaveFlag){
+						if(confirm("数据改变还未保存，是否关闭弹框？")){
+							return true;
+						}else{
+							return false;
+						}
+					}
+				}
+			},
+			end:function(){
+				dataItems = new Array();
+				index = 0;
+				$("#datagrid").datagrid("refresh");
+			}
+		});
+	}
+}
+
+/*2019.04.11演示中心页面新增*/
+//点击‘返回’按钮，弹出提示层
+function resignChange(){
+    layer.open({
+        title:'提示信息',
+        content:'确定要返回吗？',
+        area:'300px',
+        btn:['确定','取消'],
+        skin:'demo-class',
+        yes:function(index,layero){
+            layer.close(index);
+            
+        }
+    });
+}
+//新增页面，主要参观领导的新增
+function addLeader(obj){
+	var html = '';
+	html +='<tr>'+
+				'<td>'+
+					'<input type="checkbox"/>'+
+				'</td>'+
+				'<td class="addInputStyle">'+
+					'<input type="text"/>'+
+				'</td>'+
+				'<td class="addInputStyle">'+
+					'<input type="text"/>'+
+				'</td>'+
+				'<td class="addInputStyle">'+
+					'<select>'+
+						'<option>请选择</option>'+
+					'</select>'+
+				'</td>'+
+			'</tr>'
+				$(obj).parents(".contentBox").find(".visitLeader tr:last-child").after(html);
+}
+//新增页面，主要参观领导的删除
+
+/*调用选中的列表数据*/
+function delLeader(obj){
+	var checkedNumber = $(obj).parents(".contentBox").find("input[type=checkbox]:checked").length;
+    if(checkedNumber == 0){
+    	 layer.open({
+    	        title:'提示信息',
+    	        content:'请选中需要删除的数据',
+    	        area:'300px',
+    	        skin:'demo-class'
+    	    })
+    }else if(checkedNumber > 0){
+    	layer.open({
+            title:'提示信息',
+            content:'确定要删除选中的行吗？',
+            area:'300px',
+            btn:['确定','取消'],
+            skin:'demo-class',
+            yes:function(index,layero){
+                layer.close(index);
+                $(obj).parents(".contentBox").find("input[type=checkbox]:checked").parent().parent("tr").remove();
+            }
+        });
+    }
+}
