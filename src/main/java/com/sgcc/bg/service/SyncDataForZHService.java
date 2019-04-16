67dc959d6938b4d81875234a9de0c83f0b5763bb
liuchenwang <liuchenwangmegt@163.com>
2019-04-16 15:19:37 +0800

    按要求修改数据

--- src/main/java/com/sgcc/bg/service/SyncDataForZHService.java
+++ src/main/java/com/sgcc/bg/service/SyncDataForZHService.java
@@ -1,39 +1,41 @@
 package com.sgcc.bg.service;
 
+import java.util.Map;
+
 public interface SyncDataForZHService {
     /**
      * 从综合系统同步新增组织信息
      */
-    public void syncNewOrganForZH(String time,String userName);
+    public Map<String ,String> syncNewOrganForZH(String time, String userName);
 
     /**
      * 从综合系统同步部门排序信息
      */
-    public void syncDeptSortForZH(String time,String userName);
+    public Map<String ,String> syncDeptSortForZH(String time,String userName);
 
     /**
      * 从综合系统同步处室排序信息
      * @param time
      */
-    public void syncPartSortForZH(String time,String userName);
+    public Map<String ,String> syncPartSortForZH(String time,String userName);
 
     /**
      * 从综合系统同步人员排序信息
      * @param time
      */
-    public void syncEmpSortForZh(String time,String userName);
+    public Map<String ,String> syncEmpSortForZh(String time,String userName);
     /**从综合系统同步日历班次*/
-    public void syncScheduleForZH(String time,String userName);
+    public Map<String ,String> syncScheduleForZH(String time,String userName);
 
     /**
      * 从综合系统同步人员关系变更
      * @param time
      */
-    void syncEmpRelationForZH(String time,String userName);
+    Map<String ,String> syncEmpRelationForZH(String time,String userName);
 
     /**
      * 从综合系统同步部门类型
      * @param time
      */
-    void syncDeptTypeForZH(String time,String userName);
+    Map<String ,String> syncDeptTypeForZH(String time,String userName);
 }
