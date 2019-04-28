4af91ab79abfb42b9417a10ebcaa98bbd56ffaf2
wubin <wubin@mail.com>
2019-04-28 17:10:51 +0800

    获取用户管理权限  更新

--- src/main/java/com/sgcc/bg/yszx/service/impl/PrivilegeServiceImpl.java
+++ src/main/java/com/sgcc/bg/yszx/service/impl/PrivilegeServiceImpl.java
@@ -4,15 +4,21 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.sgcc.bg.yszx.bean.PrivilegeUserManager;
+import com.sgcc.bg.yszx.bean.UserPrivilege;
 import com.sgcc.bg.yszx.service.PrivilegeService;
 @Service
 public class PrivilegeServiceImpl implements PrivilegeService{
 
 	@Override
-	public List<PrivilegeUserManager> getUserManagerPrivilegeByUserName(String userName) {
+	public List<UserPrivilege> getUserManagerPrivilegeByUserName(String userName) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
+	@Override
+	public List<UserPrivilege> getApproveUserByUserName(String roleId, String deptId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
