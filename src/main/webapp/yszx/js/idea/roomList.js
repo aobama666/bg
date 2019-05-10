8a9e5ef1b1a6c1edefc8b9f121ce0170ee2dc406
caoyingjie <Cyj#930307>
2019-05-10 13:11:30 +0800

    提交

--- src/main/webapp/yszx/js/idea/roomList.js
+++ src/main/webapp/yszx/js/idea/roomList.js
@@ -6,55 +6,29 @@ var dataItems = new Array();
 var index = 0;
 roomList.btn_type_flag = 0;
 $(function(){
-
 	roomList.initDataGrid();
-	var classQuery = $(".changeQuery");
-	/* 输入框的change事件，在输入过程中自动查询  */
-	$(".changeQuery").change(function(e){
-		debugger;
-		roomList.query();
-	});
-	$(".inputQuery").on("input",function(e){
-		var valLength = e.target.value.length;
-		if(valLength>3){
-			roomList.query();
-		}
-	});
-	//回车键出发搜索按钮
-	$("body").keydown(function () {
-	    if (event.keyCode == "13") {
-	    	dataItems = new Array();
-			index = 0;
-	        $("#datagrid").datagrid("seach");
-	        return false;
-	    }
-	});
 	roomList.btn_type_flag = 0;
- 
 });
 
-/*  start  列表查询  */
+/*  start  列表查询   
 roomList.query = function(){
 	dataItems = new Array();
 	index = 0;
 	$("#datagrid").datagrid("seach");
 }
+*/
+$(".query").click(function () {
+    $("#datagrid").datagrid("seach");
+});
 /*  end  列表查询  */
 
 /* 演示中心管理-初始化列表界面  */
 roomList.initDataGrid = function(){
 	    $("#datagrid").datagrid({
-	    url: "/bg/IdeaInfo/selectIdeaInfo?tm="+new Date().getTime(),
+	    url: '/bg/IdeaInfo/selectIdeaInfo',
 		type: 'POST',
 		form:'#queryForm',		 
 		pageSize:10,
-//		showIndex:true,
-//		successFinal:function(data){
-//			todoItem.resize();
-//		},
-//		callBackFunc:function(){
-//			roomList.initItems();
-//		},
 		columns: [
 				  {name: '序号',style:{width:"1%"}, data: 'ROWNO'},         
 				  {name: '',style:{width:"1%"}, data: 'id',checkbox:true, forMat:function(row){
